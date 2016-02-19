0e40efe99e983e2b28bfe74f2809e45339a85c0c
Grzegorz <gb87@wp.pl>
2016-02-19 17:43:29 +0100

    Fix JS bug with empty array

--- app/assets/javascripts/cars.js
+++ app/assets/javascripts/cars.js
@@ -3,16 +3,20 @@ $(document).ready(function() {
 
   $(function () {
     var arr = gon.dates;
+    if (typeof arr !== 'undefined' && arr.length > 0) {
     arr = jQuery.map(arr, function(date) {
       return new Date(date*1000);
     });
+  } else {
+    var errorList = "";
+  }
     $('#datetimepicker12').datetimepicker({
       disabledDates: arr,
       format: "dd MM yyyy",
       inline: true,
       showTodayButton: true,
       });
-    $('#refresh_button').click(function(){
+      $('#refresh_button').click(function(){
       location.reload(true);
     });
   });
